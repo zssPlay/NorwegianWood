d05589bbce6d737935d62bdb659d0d7ecc0a1241
zhaoshanshan <zss2691637900@163.com>
2022-12-27 17:50:28 +0800

    12.27 17:50

--- src/main/java/com/wood/util/demo.java
+++ src/main/java/com/wood/util/demo.java
@@ -1,12 +1,8 @@
 package com.wood.util;
 
 
-
 public class demo {
     public static void main(String[] args) {
-        int i = 0;
-        do{
-           i++;
-        }while (i < 10);
+
     }
 }
